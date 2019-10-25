5b875086dc208334d5e168f4319637ffdca54242
yadimr <yadimr07@gmail.com>
2019-10-25 14:44:35 +0700

    Showing data from elevania page 1, just showing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,41 +1,61 @@
 import React from 'react';
 import '../css/card.css';
 
+
 class CardHeader extends React.Component {
     render() {
-      const { image } = this.props;
+      const { images } = this.props;
       var style = { 
-          backgroundImage: 'url(' + image + ')'
+          backgroundImage: 'url(' + images + ')'
       };
       return (
-        <header style={style} id={image} className="card-header">
+        <header style={style} id={images} className="card-header">
         </header>
       )
     }
   }
   
-  class Button extends React.Component {
-    render() {
-      return (
-        <button className="button button-primary">
-          <i className="fa fa-chevron-right"></i> Find out more
-        </button>
-      )
-    }
-  }
-  
   class CardBody extends React.Component {
+    constructor(props){
+      super(props);
+
+      this.state = {
+          title : this.props.title,
+          harga : this.props.harga,
+          stok : this.props.stok,
+          desc : this.props.desc,
+          // btnDisable : true
+      };
+  } 
+
+    handleChange (e) {
+      
+      const name = e.target.name;
+      const value = e.target.value;
+      this.setState({[name]: value})
+      
+    }
+
     render() {
       return (
         <div className="card-body">
-          <p className="date">Stok Tersedia: 0</p>
+          <label htmlFor='text'>Stok tersedia :</label>
+          <input type="number" name='stok' value = {this.state.stok} onChange={(event) => this.handleChange(event)}/>
+          {/* <p className="date">Stok Tersedia: {this.props.stok}</p> */}
           
-          <h2>{this.props.title}</h2>
+          {/* <h4 className="card-title">{this.props.title}</h4> */}
+          <label htmlFor='title'>Nama produk :</label>
+          <input type='text' name='title' value={this.state.title} onChange={(event) => this.handleChange(event)}/>
+
+          <label htmlFor='desc'>Deskripsi :</label>
+          <textarea name='desc' value={this.state.desc} onChange={(event) => this.handleChange(event)}></textarea>
+          {/* <p className="body-content">{this.props.desc}</p> */}
           
-          <p className="body-content">{this.props.text}</p>
-          <p className="harga-coontent">Rp. 1.200.000</p>
+          <br/><label htmlFor='harga'>Harga :</label>
+          <br/><input type='number' name='harga' value={this.state.harga} onChange={(event) => this.handleChange(event)}/>
+          {/* <p className="harga-coontent">Rp. {this.props.harga}</p> */}
           
-          <Button />
+          <button type='button' className='saveButton'>Simpan</button>
         </div>
       )
     }
@@ -43,12 +63,16 @@ class CardHeader extends React.Component {
   
   class Card extends React.Component {
     render() {
-        const {image} = this.props;
         const {title} = this.props;
+        const {id} = this.props;
+        const {harga} = this.props;
+        const {stok} = this.props;
+        const {desc} = this.props;
+        const {images} = this.props;
       return (
-        <article className="card">
-          <CardHeader image={'https://source.unsplash.com/user/erondu/600x400'}/>
-          <CardBody title={title} text={'Kayaks crowd Three Sister Springs, where people and manatees maintain controversial coexistence'}/>
+        <article className="card" id={id}>
+          <CardHeader images={images}/>
+          <CardBody title={title} harga={harga} stok={stok} desc={desc}/>
         </article>
       )
     }
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,47 +1,83 @@
 import React from 'react';
 import '../css/main-wrapper.css';
 import Card from './Card';
+import Axios from 'axios';
 
 class MainView extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-
+            response_div : []
         };
     }
 
-    fetchSentiment = () => {
-        fetch('http://localhost:5000/apis', {
-            method : 'GET'
-            })
-            .then(response => response.json())
-            .then(responseJSON => {
-                console.log(responseJSON);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    fetchSentiment = async () => {
+        const datas = await this.getDataElv();
+        const XMLParser = require('react-xml-parser');
+        const xml = new XMLParser().parseFromString(datas);
+        const dataLength = xml.children.length;
+        const arrs = [];
+
+  
+        for (let i = 0; i < dataLength; i++) {
+            arrs.push([])
+        }
+
+        for (let i = 0; i < dataLength; i++) {
+            
+            var prdNo = xml.children[i].getElementsByTagName('prdNo')[0]['value'];
+            const dataProd = await this.getProdDet(prdNo);
+            const xml2 = new XMLParser().parseFromString(dataProd);
+
+            var selPrc = xml2.getElementsByTagName('selPrc')[0]['value'];
+            var prdSelQty = xml2.getElementsByTagName('prdSelQty')[0]['value'];
+            var prdNm = xml2.getElementsByTagName('prdNm')[0]['value'];
+            var htmlDetail = xml2.getElementsByTagName('htmlDetail')[0]['value'];
+            var prdImage01 = xml2.getElementsByTagName('prdImage01')[0]['value'];
+            // var prdNo = xml2.children[i].getElementsByTagName('prdNm')[0]['value'];
+            
+            arrs[i].push(selPrc, prdSelQty,prdNm,htmlDetail,prdImage01);
+            
+        }
+        this.setState({
+            response_div : arrs
+        });
+        
+        // console.log(selPrc);
+        return null;
+
+    }
+
+    getDataElv = async () => {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "http://api.elevenia.co.id/rest/prodservices/product/listing?page=1";
+        const ucup = '721407f393e84a28593374cc2b347a98';
+        let res = await Axios.get(proxyurl +url, {headers :{'openapikey' : ucup}});
+        let data = res.data;
+        // console.log(this.state.here);
+        return data;
+    }
+
+    getProdDet = async (prodNum) => {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "http://api.elevenia.co.id/rest/prodservices/product/details/"+prodNum;
+        const ucup = '721407f393e84a28593374cc2b347a98';
+        let res = await Axios.get(proxyurl +url, {headers :{'openapikey' : ucup}});
+        let data = res.data;
+        // console.log(this.state.here);
+        return data;
     }
 
     render() {
-
+        
         return (
             <div>
                 <h1>Jubelio</h1>
                 <div className='main-wrapper'>
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
-                    <Card title={'Celana XXL'} />
+                    {this.state.response_div.map((title,key) => (
+                        <Card key = {key} title = {title[2]} harga={title[0]} stok={title[1]} desc={title[3]} images={title[4]}/>
+                    ))}
                 </div>
                 <div className='button-wrapper'>
                     <button type='button' onClick={this.fetchSentiment}>Get Data</button>
